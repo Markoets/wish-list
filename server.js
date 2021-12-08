2cebf3e3a1d857a7512b55ec2c0dc7d34d953cda
Opilane TTHK <opilane@tthk.ee>
2021-12-08 14:37:21 +0200

    Add 404 page and day

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const date = require(__dirname+'/getDate.js')
 
 const app = express();
+app.use(express.static('public'))
 
 app.get('/',(request,response)=>{
     response.send('Hello, world!')
 });
 
+app.get('/getdate',(req,res)=>{//req - request , res-response
+    let today = date.getDate();
+    res.send(today);
+})
+
+app.get('/weekday',(req,res)=>{
+    let weekday = date.getWeekDay();
+    res.send(weekday);
+})
 
 
 
+
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+"/404.html")
+})
 port = 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
