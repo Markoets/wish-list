bc3ec9d76e3578a3972913393491ebaf5b0e106a
Opilane TTHK <opilane@tthk.ee>
2021-12-09 13:42:04 +0200

    Add wishes

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require('express');
-const date = require(__dirname+'/getDate.js')
+const mainRoutes = require ('./routes/mainRoutes');
+const errorRoute = require('./routes/errorRoute')
 
 const app = express();
-app.use(express.static('public'))
+app.set('view engine','ejs'); 
+app.use(express.urlencoded({extended:true}))  //get data from request body
+app.use(express.static('public'));
 
-app.get('/',(request,response)=>{
-    response.send('Hello, world!')
-});
+app.use(mainRoutes);
+app.use(errorRoute);
 
-app.get('/getdate',(req,res)=>{//req - request , res-response
-    let today = date.getDate();
-    res.send(today);
-})
-
-app.get('/weekday',(req,res)=>{
-    let weekday = date.getWeekDay();
-    res.send(weekday);
-})
-
-
-
-
-app.get('*',(req,res)=>{
-    res.sendFile(__dirname+"/404.html")
-})
 port = 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
